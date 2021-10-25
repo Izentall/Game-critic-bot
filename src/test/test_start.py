979505075e6f789e99e63cc6bd93ed35ad7d1338
Izentall <kichurqq@gmail.com>
2021-10-25 17:18:59 +0300

    Fix test for /start Add file to .gitignore

--- src/test/test_start.py
+++ src/test/test_start.py
@@ -3,12 +3,13 @@ from secrets import token_urlsafe
 from telethon import TelegramClient
 from telethon.tl.custom.message import Message
 from telethon.tl.types.messages import BotCallbackAnswer
+from src.constants import (keyboard_MENU, greetings_text, using_buttons_text)
 
 
 @mark.asyncio
 async def test_start(client: TelegramClient):
     # Create a conversation
-    with client.conversation("@GameMetaCriticBot", timeout=5) as conv:
+    async with client.conversation("@GameMetaCriticTestBot", timeout=5) as conv:
         # User > /start
         await conv.send_message("/start")
         # Bot < Привет, {user}!
@@ -21,13 +22,12 @@ async def test_start(client: TelegramClient):
         # Bot < Я информационный бот, который поможет тебе узнать оценки рецензии на различные видеоигры!
         # Чтобы получить информацию, просто напиши название игры!
         resp = await conv.get_response()
-        assert "Я информационный бот" in resp.raw_text
-        assert "Чтобы получить информацию" in resp.raw_text
+        assert greetings_text in resp.raw_text
 
         # Bot < Или воспользуйся кнопками:
         # [Топы Игр][Платформы]
         resp = await conv.get_response()
-        assert "Или воспользуйся кнопками" in resp.raw_text
-        assert resp.button_count == 2
-        assert resp.buttons[0][0].text == "Топы Игр"
-        assert resp.buttons[0][1].text == "Платформы"
+        assert using_buttons_text in resp.raw_text
+        assert resp.button_count == len(keyboard_MENU[0])
+        assert resp.buttons[0][0].text == keyboard_MENU[0][0].text
+        assert resp.buttons[0][1].text == keyboard_MENU[0][1].text
